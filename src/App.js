bed5b1b0695b5977251658b13c8c79c0a8408c7c
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 22:13:33 +0600

    Custom component; Giving the component the props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
+{/*props: props allows us to pass data from one component to another dynamically*/}
+
 
 {/*here Person is a component*/}
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-      <h1>First Name: Dalia</h1>
-      <h1>Last Name: Makhmal</h1>
-      <h1>Age: 20</h1>
+      <h1>First Name: {props.firstName}</h1>
+      <h1>Last Name: {props.lastName}</h1>
+      <h1>Age: {props.age}</h1>
     </>
   )
 }
@@ -18,11 +20,13 @@ const App = () => {
     <div className="App">
       <h1>Hello World</h1>
       {/*And we can call the component*/}
-      <Person/>   
-      <Person/>
-      <Person/>
-      <Person/>
-      <Person/>
+      <Person firstName='Dalia' lastName='Makhmal' age='20' />
+      {/*Here firstName, lastName and age are props or properties or attributes of this person component*/}
+      <Person firstName='Lucy' lastName='Heartfiliya' age='24'/>
+      <Person firstName='Erzza' lastName='Scarlet' age='27'/>
+      <Person firstName='Windy' lastName='Marvel' age='17'/>
+      <Person firstName='Anna' lastName='Heartfiliya' age='345'/>
+      <Person firstName='Mavis' lastName='vermilion' age='320'/>
     </div>
   );
 }
