0d1d06ab904130d73b0eea684854b15093f9208b
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 21:26:58 +0600

    How ternary expression works

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ function App() {
   const isNameShowing = false;
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name: 'World'}! @_@</h1>
+      <h1>Hello, {2 + 2 + 2 * 8}! @_@</h1>
+      
+      {//ternary expression:  if name exists
+        name ? (
+          <>
+            <h1>exicute this part of the code</h1>
+          </>
+        ) : (
+            <>
+              <h1> otherwise exicute this part of the code</h1>
+            </>
+        )
+      }
     </div>
   );
 }
