6b93b9b2997e3ff64d7a6533fab5dd628a508e88
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 22:39:09 +0600

    React states and setterfunction

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 
-{/*props: props allows us to pass data from one component to another dynamically*/}
-
-
-{/*here Person is a component*/}
-const Person = (props) => {
-  return (
-    <>
-      <h1>First Name: {props.firstName}</h1>
-      <h1>Last Name: {props.lastName}</h1>
-      <h1>Age: {props.age}</h1>
-    </>
-  )
-}
+{/*react-state: a plain JS object used by the React component to represent a piece of information about the current state of the component. it is complately managed by the React component itself.*/ }
+{/*whenever we call something as a function  and it starts with 'use', it's a hook*/ }
 
 
 const App = () => {
+  {/*a concept of array destructuring
+  const [name Of The State, set Function Of The State or Setterfunction] = useState();*/}
+  const [counter, setCounter] = useState();
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      {/*And we can call the component*/}
-      <Person firstName='Dalia' lastName='Makhmal' age='20' />
-      {/*Here firstName, lastName and age are props or properties or attributes of this person component*/}
-      <Person firstName='Lucy' lastName='Heartfiliya' age='24'/>
-      <Person firstName='Erzza' lastName='Scarlet' age='27'/>
-      <Person firstName='Windy' lastName='Marvel' age='17'/>
-      <Person firstName='Anna' lastName='Heartfiliya' age='345'/>
-      <Person firstName='Mavis' lastName='vermilion' age='320'/>
+      <h1>A Counter</h1>
+      <button>+</button>
+      <h1>{counter}</h1>
+      <button>-</button>
     </div>
   );
 }
