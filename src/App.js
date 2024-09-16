81e57fed072d6184a80be782608e0522c6ff09d2
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 22:49:11 +0600

    A call-back function

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 import { useState } from 'react';
 
 {/*react-state: a plain JS object used by the React component to represent a piece of information about the current state of the component. it is complately managed by the React component itself.*/ }
+{/*useState: a hook that lets you add state to a functional component*/ }
 {/*whenever we call something as a function  and it starts with 'use', it's a hook*/ }
 
+{/*events: some kind of system generated event */ }
+
+{/*a call-back function: a function that simply does not have a name, its there and its waiting for some kind of command*/ }
 
 const App = () => {
   {/*a concept of array destructuring
   const [name Of The State, set Function Of The State or Setterfunction] = useState();*/}
   const [counter, setCounter] = useState();
   return (
-    <div className="App">
+    <div className='App'>
       <h1>A Counter</h1>
-      <button>+</button>
+      <button onClick={() => alert('clicked')}>
+        +
+      </button>
       <h1>{counter}</h1>
       <button>-</button>
     </div>
