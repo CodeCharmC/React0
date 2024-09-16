4d60011bf750d5523ed4ca9f6e57ad3adb93de66
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 21:29:45 +0600

    Example1 of ternary expression

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ function App() {
     <div className="App">
       <h1>Hello, {2 + 2 + 2 * 8}! @_@</h1>
       
-      {//ternary expression:  if name exists
+      {//ternary expression:
         name ? (
           <>
-            <h1>exicute this part of the code</h1>
+            <h1>{name}</h1>
           </>
         ) : (
             <>
-              <h1> otherwise exicute this part of the code</h1>
+              <h1>There is no name</h1>
+              <h6>Thank you</h6>
             </>
         )
       }
