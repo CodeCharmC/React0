0cd469dcd54fcaac7f00aee94161611d769e855a
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-16 21:05:22 +0600

    Ternary expression and making application dynamic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 function App() {
   //create a variable
   const name = 'Dalia';
+
+  //diffrent ternary expression
+  const isNameShowing = false;
   return (
     <div className="App">
-      <h1>Hello, {name}! @_@</h1>
+      <h1>Hello, {isNameShowing ? name: 'World'}! @_@</h1>
     </div>
   );
 }
 
 export default App;
+
