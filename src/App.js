52d040872cb0055ac3cff468619e01ae0e76da67
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-17 08:06:50 +0600

    Creating basic setup for REACT0 project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-
-
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  {/*a variable  related to a hook can be changed only by using its own setter function*/}
-
-  useEffect(() => {
-    setCounter(100);
-  }, []); {/*[] here is a deppendency array*/}
-
-  return (
-    <div className='App'>
-      <h1>A Counter</h1>
-      <button onClick={() => setCounter((prevCount) => (prevCount + 1))}>
-        +
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount-1)}>
-        -
-      </button>
-    </div>
-  );
+   return (
+      <h1>App</h1>
+   );
 }
 
 export default App;
-
