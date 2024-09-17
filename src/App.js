bb785129784745a8222ff38f7ae67bd1e15e8d9e
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-09-17 08:26:43 +0600

    Connecting to omdb api and searching for movies by its title and load them in console as soon as we load in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import { useEffect } from 'react';
+
+//e3768f
+const API_URL = 'https://www.omdbapi.com/?apikey=e3768f';
 
 const App = () => {
+
+   const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+
+      console.log(data)
+   }
+
+   useEffect(() => {
+      searchMovies('spiderman');
+   }, []);
+
    return (
       <h1>App</h1>
    );
